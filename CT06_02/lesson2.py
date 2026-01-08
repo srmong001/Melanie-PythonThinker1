4fe60df944d881d0d1e6b1900d70a3b4790705fd
srmong001 <melanieong01@gmail.com>
2026-01-08 11:00:38 +0000

    Lesson 1

--- CT06_02/lesson2.py
+++ CT06_02/lesson2.py
@@ -1,7 +1,55 @@
-print("Hello from lesson 2")
+# Recap 1 
+print("Red")
+print("Orange")
+print("Yellow")
+print("Green")
+print("Blue")
+print("Indigo")
+print("Violet")
 
-######## Write the pseudocode in comments for task 2 here
-# Using comments, translate the code shown on screen into pseudocode.
+# print("Red")
+# print("Orange")
+# print("Yellow")
+print("Green")
+# print("Blue")
+# print("Indigo")
+# print("Violet")
 
-######## Write the pseudocode in comments for task 3 here
+
+# Task 1a
 # Using comments, translate the code shown on screen into pseudocode.
+# repeat 10 times : say Hey! , move 10 times
+
+# Task 1b
+# Using comments, translate the code shown on screen into pseudocode.
+# set counter to 0
+# repeat until counter = 50 : move 10 steps, turn 15 degrees then change counter by 10
+
+# Task 1c
+# Ask "what is you age"
+# If answer < 18 then say "Access Denied" Else, say "Welcome"
+
+# Task 2
+
+# Start with robot picking the item up.
+# If item is made of glass, put into glass bin
+# Else check if item is made of plastic then put into plastic bin
+# Else check if item is made of paper then put into paper bin
+# Repeat until items are items are done sorting.
+
+# Task 3
+# Check if user entered "right secret phrase"
+# If answer is "wrong secret phrase" then send "Error message" 
+# Else if answer is "right secret phrase" then send "Congratulation"
+
+# Task 4
+# Start by calculating each individual test.
+# For test 1, calculate 20% of test 1 final score
+# For test 2, calculate 40% of test 2 final score
+# For test 3, calculate 40% of test 3 final score
+# Sum up for the final score
+
+# test 1 = test 1 x 0.2
+# test 2 = test 2 x 0.4
+# test 3 = test 3 x 0.4
+# student's final score = test 1 + test 2 + test 3 
