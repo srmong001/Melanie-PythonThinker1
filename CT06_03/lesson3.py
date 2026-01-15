d868a1b720d180a3512d683711286a3cca7cffe6
srmong001 <melanieong01@gmail.com>
2026-01-15 11:05:31 +0000

    Lesson 2

--- CT06_03/lesson3.py
+++ CT06_03/lesson3.py
@@ -1,1 +1,98 @@
-print("Hello from lesson 3")
+import math 
+
+# my_string="Helloooo"
+# print(my_string)
+# my_number =100
+# print(my_number)
+# Task 1a
+# age=24
+# print(age)
+# print("age")
+
+# # Task1b
+# x=10
+# x=20
+# print(x)
+
+# Task2a
+# variable1 = 3
+# variable2 = 5
+# print(variable1 + variable2)
+# print(variable1 * variable2)
+# print(variable1 / variable2)
+# print(variable1 - variable2)
+
+# Task3a
+# num1 = 42.56789
+# print(round(num1,2))
+
+# Task3b
+# num2 = 1267.15
+# print(round(num2))
+
+# Task3c
+# num3 = 1785.956
+# print(round(num3,-2))
+
+# Task4a
+# a = 5
+# b = 9
+# print(a + b)
+
+# Task4b
+# ans = (10 - 50) * 3
+# print(ans)
+
+# Task4c
+# ans = 100 / 4
+# print(ans)
+
+# Task4d
+# ans = (3 + 4)* 5 - (10 / 2)
+# print(ans)
+
+# Task5a
+# pineapple2 = 15 * 2
+# bakkwa = 58
+# print(pineapple2 + bakkwa)
+
+# Task5b
+# seafood = 96
+# sides = 18 * 3
+# rice = 2 * 6
+# total = print(seafood + sides + rice)* 1.1 - 25
+# print(round(total,2))
+# to redo 5b
+
+# Task5c
+# seafood = 100 * 5
+# sides = 20 * 8
+# rice = 2.50 * 20
+# total = (seafood + sides + rice) * 1.1 * 1.09
+# print(round(total,2))
+
+# Task6a
+# distance = abs(-55)
+# print(distance)
+
+# Task6b
+# degrees = abs(-8)
+# print(degrees)
+
+# Task7a
+# max_batch = math.floor(9.8)
+# print(max_batch)
+
+# Task7b
+# num_of_bag = 27 / 4
+# print(num_of_bag)
+# min_bags = math.ceil(6.75)
+# print(min_bags)
+
+# Task8a
+# distance = math.pi * 100
+# print(round(distance,2))
+
+# Task8b
+# total_area = math.pi * 12 ** 2
+# print(round(total_area,2))
