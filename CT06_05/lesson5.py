c397806e3f894a26bf35050a1bdafc1e6c6398b1
srmong001 <melanieong01@gmail.com>
2026-01-29 11:05:54 +0000

    Lesson 4

--- CT06_05/lesson5.py
+++ CT06_05/lesson5.py
@@ -1,1 +1,102 @@
-print("Hello from lesson 5")
+#  print("Hello from lesson 5")
+# name = input("Enter your name:")
+# age = input("Enter your age:")
+# message = input("Enter your personal message:")
+# print(name + age + message) 
+# print("Happy " + age + "th birthday " + name + "! " + message)
+# print(f"Happy {age}th birthday {name}! {message}")
+
+# Loops 
+# Task 1
+# for i in range(100):
+#     print("I like chicken rice")
+
+#  Task 2
+# for i in range(100):
+#     print("I like cake")
+#     print("give me more")
+
+# iterator
+# for i in range (5):
+#     print (i)
+
+# iterator
+# for i in range (10, 20, 2):
+#     print (i)
+
+#    iterator
+# for i in range (100, 0, -1):
+#     print (i) 
+
+# Task 3
+# for i in range (60):
+#     print(i)
+
+# Task 4a
+# for i in range(1, 6, 1):
+#     print(i)
+
+# Task 4b
+# for i in range(51,101,1):
+#     print(i)
+
+# Task 4c
+# for i in range(18,30,1):
+#     print(i)
+
+# Task 5a
+# for i in range(2, 26, 2):
+#     print(i)
+
+# Task 5b
+# for i in range(8, 104, 8):instead of 104 , 97 also ok
+#     print(i)
+
+# Task 5c
+# for i in range (6, 0, -1):
+#     print(i)
+
+# Task 6a
+# print("Ready!")
+# for i in range(3, 0, -1):
+#     print(i)
+
+# Task 6b
+# for i in range(10, 0, -1):
+#     print(i)
+# print("Boo!")
+
+# Task 7
+# start = input("Enter a starting number:")
+# start = int(start)
+# stop = input("Enter a ending number:")
+# stop = int(stop)
+# for i in range(start, stop + 1):
+#     print(i)
+
+# Task 8
+# for i in range (2, 41, 2):
+    # print(i)
+# total = 0
+# for i in range(2,41):
+#     total = total + 2
+# print (total) 
+# ^^ wrong
+
+# total = 0
+# for i in range(2, 41, 2):
+#     total = total + i
+# print(total)
+
+# Task 8b
+# total = 0
+# for i in range(9, 100, 2):
+#     print(i)
+#     total = total + i
+# print(total)
+
+word = "DAVE"
+for char in word:
+    print("Give me a " + char + "!")
+print("What do we have?")
+print("Dave is the best!")
